d238884fb2bae86f3b593acd50073569ce2b020f
Matteo Angrisano <matteoangrisano@AIR-DI-MATTEO.station>
2023-10-05 17:52:34 +0200

    appsync refactoring

--- serverless/appsync/resolvers.ts
+++ serverless/appsync/resolvers.ts
@@ -5,7 +5,6 @@ const resolvers = {
   "Query.getArticle": {
     kind: "UNIT",
     code: `${queriesFolder}/getArticle.ts`,
-    request: "",
     dataSource: "tableArticles",
   },
   "Mutation.postArticle": {
