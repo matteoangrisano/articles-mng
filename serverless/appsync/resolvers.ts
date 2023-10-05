be139d28209dbffeec066fc26fa9f27930b0ad47
Matteo Angrisano <matteoangrisano@AIR-DI-MATTEO.station>
2023-10-05 21:53:39 +0200

    appsync refactoring

--- serverless/appsync/resolvers.ts
+++ serverless/appsync/resolvers.ts
@@ -4,14 +4,18 @@ const mutationsFolder = "mappingTemplates/mutations";
 const resolvers = {
   "Query.getArticle": {
     functions: [
-      { dataSource: "tableArticles" },
-      { code: `${queriesFolder}/getArticle.ts` },
+      {
+        dataSource: "tableArticles",
+        code: `${queriesFolder}/getArticle.ts`,
+      },
     ],
   },
   "Mutation.postArticle": {
     functions: [
-      { dataSource: "tableArticles" },
-      { code: `${mutationsFolder}/postArticle.ts` },
+      {
+        dataSource: "tableArticles",
+        code: `${mutationsFolder}/postArticle.ts`,
+      },
     ],
   },
 };
