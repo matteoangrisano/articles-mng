100b9549699f2b9d5ab1e2906e79f454b5d5088a
Matteo Angrisano <matteoangrisano@AIR-DI-MATTEO.station>
2023-10-18 21:59:16 +0200

    test lambda .env

--- serverless/src/functions/hello/handler.ts
+++ serverless/src/functions/hello/handler.ts
@@ -2,10 +2,6 @@ import type { ValidatedEventAPIGatewayProxyEvent } from "@libs/api-gateway";
 import { formatJSONResponse } from "@libs/api-gateway";
 import { middyfy } from "@libs/lambda";
 
-import "dotenv";
-import { config } from "dotenv";
-config({ path: `${__dirname}/../../../../../.env` });
-
 import schema from "./schema";
 
 const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (
