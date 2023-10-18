f3967f9f2c1edd750373562dc0758a08ba764c81
Matteo Angrisano <matteoangrisano@AIR-DI-MATTEO.station>
2023-10-18 22:35:57 +0200

    test lambda .env

--- serverless/src/functions/hello/index.ts
+++ serverless/src/functions/hello/index.ts
@@ -3,6 +3,6 @@ import { handlerPath } from "@libs/handler-resolver";
 export default {
   handler: `${handlerPath(__dirname)}/handler.call`,
   environment: {
-    REGION: "${ENV:REGION}",
+    REGION: process.env.REGION,
   },
 };
